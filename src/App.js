c102271dcc840a58463c7ba383be0064b219949e
Kaz Taz <huatienhao095@gmail.com>
2023-08-05 18:51:05 +0700

    Fixing Helmet

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, { useState, useEffect } from 'react';
 import { commerce } from './lib/commerce'
 import { Products, Navbar, Cart, Checkout } from './components';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Helmet } from 'react-helmet';
 
 
 const App = () => {
